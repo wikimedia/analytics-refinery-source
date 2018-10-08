d45263e3456923de54d06b9c0324144c30d700d9
Joseph Allemandou <joal@wikimedia.org>
2018-10-08 21:57:14 +0200

    Add webrequest_subset_tags transform function
    This function is to be used with DataFrameToHive utility.
    It joins a dataframe having a 'tags' column (webrequest) to
    the wmf.webrequest_subset_tags table, on tags. The resulting
    dataframe is then dynamically partitionned over the new 'subset'
    column.
    
    Bug: T164020
    Change-Id: I153ff3e7c9579424dd93ad50cd8365022bc6d1bb

--- refinery-job/src/test/scala/org/wikimedia/analytics/refinery/job/refine/TestTransformFunctions.scala
+++ refinery-job/src/test/scala/org/wikimedia/analytics/refinery/job/refine/TestTransformFunctions.scala
@@ -21,7 +21,13 @@ case class TestData(
     uuid: String = "2a9de856da6a5fdc82a7f19f2b5cf99c",
     meta: SubObject = SubObject(),
     //IPv4 addresses taken from MaxMind's test suite
-    ip: String = "81.2.69.160"
+    ip: String = "81.2.69.160",
+    tags: Array[String] = Array("tag1", "tag2")
+)
+
+case class WebrequestSubsetTags(
+    webrequest_tags: Array[String],
+    webrequest_subset: String
 )
 
 class TestTransformFunctions extends FlatSpec
@@ -31,11 +37,7 @@ class TestTransformFunctions extends FlatSpec
     val data2 = TestData(uuid="b0f7848f973f5e90ac531055b45bfeba")
     val data3 = TestData(meta=SubObject(id="f6770c245746515089a6237f9fa6536b"))
 
-    val fakeHivePartition = new HivePartition(database = "testDb", t = "testTable", location = "/fake/location")
-
-    val partition: HivePartition = HivePartition(
-        "my_db", "my_table", "/path/to/table", ListMap("year" -> Some("2018"))
-    )
+    val fakeHivePartition = new HivePartition(database = "testDb", t = "testTable", location = "/fake/location", ListMap("year" -> Some("2018")))
 
     it should "deduplicate_eventlogging based on uuid" in {
         val partDf = new PartitionedDataFrame(spark.createDataFrame(sc.parallelize(Seq(data1, data1, data2))), fakeHivePartition)
@@ -55,6 +57,29 @@ class TestTransformFunctions extends FlatSpec
         transformedDf.df.columns.contains("geocoded_data") should equal(true)
 
         transformedDf.df.select("geocoded_data.continent").take(1).head.getString(0) should equal("Europe")
+    }
+
+    it should "join a dataframe to webrequest_subset_tags on tags" in {
+        val subsetTags = Seq(
+            WebrequestSubsetTags(Array("tag1"), "subset1"),
+            WebrequestSubsetTags(Array("tag3"), "subset2"),
+            WebrequestSubsetTags(Array("tag1", "tag2"), "subset3")
+        )
+        spark.createDataFrame(sc.parallelize(subsetTags)).createOrReplaceTempView("test_subset_tags")
+        val partDf = new PartitionedDataFrame(spark.createDataFrame(sc.parallelize(Seq(data1))), fakeHivePartition)
+        val pws = partition_webrequest_subset
+        pws.webrequestSubsetTagsTable = "test_subset_tags"
+        val transformedDf = pws(partDf)
+        val transformedValues = transformedDf.df.collect()
+        val transformedPartition = transformedDf.partition
+        // The single row is has 2 subsets --> it gets duplicated
+        transformedValues.length should equal(2)
+        // subset is expected to be at 6th position in row
+        transformedValues.map(_.getString(5)) should contain("subset1")
+        transformedValues.map(_.getString(5)) should contain("subset3")
+        // Partition should have 2 elements, subset being last and without value (dynamic)
+        transformedPartition.partitions.size should equal(2)
+        transformedPartition.partitions("subset") should equal(None)
     }
 
 }
