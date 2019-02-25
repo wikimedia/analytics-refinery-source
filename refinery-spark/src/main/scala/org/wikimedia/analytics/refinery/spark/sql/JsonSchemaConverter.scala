c541d7a57ee79afcfa49bb3b7bad8e4ae593d754
Joseph Allemandou <joal@wikimedia.org>
2019-02-25 20:48:30 +0100

    Add JsonSchemaConverter to spark package
    This patch adds a utility function to convert a subset of json schema
    definition to spark dataframe schema.
    
    Bug: T215442
    Change-Id: I80663e9b9bcb8bdaaae1ebc2ccff65e460a69426

---
+++ refinery-spark/src/main/scala/org/wikimedia/analytics/refinery/spark/sql/JsonSchemaConverter.scala
@@ -0,0 +1,161 @@
+package org.wikimedia.analytics.refinery.spark.sql
+
+import com.fasterxml.jackson.databind.JsonNode
+import org.apache.spark.sql.types._
+
+import scala.collection.JavaConverters._
+import scala.util.Try
+
+/**
+ * Defines functions to convert a subset of JSONSchema to a spark StructType.
+ *
+ * Notes:
+ * - Only a subset of JSONSchema is supported.
+ * - object with additionalProperties only are converted to a Map.
+ */
+object JsonSchemaConverter {
+
+    // Useful JSONSchema field names
+    private val typeField                    = "type"
+    private val itemsField                   = "items"
+    private val propertiesField              = "properties"
+    private val additionalPropertiesField    = "additionalProperties"
+    private val requiredField                = "required"
+    private val descriptionField             = "description"
+
+
+    /*
+     * Build an ArrayType from an array JSONSchema node.
+     * The JsonNode is an object (checked in buildDataType)
+     * and is expected to contain a valid items field with type.
+     *
+     * This function recursively calls the buildStructType function to get items type.
+     */
+    private def buildArrayType(jsonSchema: JsonNode, fieldName: String): ArrayType = {
+        val itemsSchema = jsonSchema.get(itemsField)
+        // Arrays must specify the type of their elements.
+        if (itemsSchema == null || itemsSchema.isNull)
+            throw new IllegalArgumentException(s"$fieldName array schema did not specify the items field")
+        // Arrays must only use a single type, not tuple validation.
+        if (!itemsSchema.isObject || !itemsSchema.has(typeField)) {
+            throw new IllegalArgumentException(s"$fieldName array schema must specify the items type field")
+        }
+        ArrayType(buildDataType(itemsSchema, s"$fieldName-$propertiesField"), containsNull = false)
+    }
+
+    /*
+     * Build a MapType from an object JSONSchema node without properties field.
+     * The JsonNode is an object (checked in buildDataType) and is expected
+     * to contain a valid additionalProperties field with type.
+     *
+     * This function recursively calls the buildStructType function to get items type.
+     */
+    private def buildMapType(jsonSchema: JsonNode, fieldName: String): MapType = {
+        val additionalPropertiesSchema = jsonSchema.get(additionalPropertiesField)
+        if (additionalPropertiesSchema == null || additionalPropertiesSchema.isNull)
+            throw new IllegalArgumentException(
+                s"$fieldName object schema must specify either properties or additionalProperties field")
+        if (!additionalPropertiesSchema.isObject || !additionalPropertiesSchema.has(typeField))
+            throw new IllegalArgumentException(
+                s"$fieldName object schema additionalProperties field must specify its type")
+        MapType(
+            StringType,
+            buildDataType(additionalPropertiesSchema, s"$fieldName-$additionalPropertiesField"),
+            valueContainsNull = false)
+    }
+
+    /*
+     * Build a DataType from a type-describing JsonNode and a field-name for better logging.
+     * The JsonNode is expected to contain a valid JSONSchema type TextNode.
+     *
+     * Note: A special case of converting object with only additionalProperties to a Map is built-in.
+     *
+     * This function recursively calls itself to get array-items types, and the
+     * buildStructType function to get object inner types.
+     */
+    private def buildDataType(jsonSchema: JsonNode, fieldName: String): DataType = {
+        if (jsonSchema == null ||  jsonSchema.isNull)
+            throw new IllegalArgumentException("Schema node shouldn't be null when building DataType")
+        if (!jsonSchema.isObject)
+            throw new IllegalArgumentException("Schema node should be an object when building DataType")
+
+        val schemaTypeNode = jsonSchema.get(typeField)
+        if (schemaTypeNode == null || !schemaTypeNode.isTextual)
+            throw new IllegalArgumentException("Schema must contain 'type' field")
+
+        val typeText = schemaTypeNode.textValue()
+        typeText match {
+            case "null" => NullType
+            case "boolean" => BooleanType
+            case "integer" => IntegerType
+            case "number" => DoubleType
+            case "string" => StringType
+            case "array" => buildArrayType(jsonSchema, fieldName)
+            case "object" =>
+                val properties = jsonSchema.get(propertiesField)
+                // Regular sub-object case: properties subfield is defined
+                if (properties != null && !properties.isNull) buildStructType(jsonSchema, fieldName)
+                // Special map-case: properties undefined, additionalProperties defined with type
+                else buildMapType(jsonSchema, fieldName)
+            case _ => throw new IllegalArgumentException(s"$fieldName has invalid type value: $typeText")
+        }
+    }
+
+    /*
+     * Build a StructField from a JsonNode, its field-name, and its mandatoriness.
+     * The JsonNode is expected to contain a valid JSONSchema type as subfield.
+     *
+     * This function recursively calls  the buildDataType function to get field type.
+     */
+    private def buildStructField(jsonSchema: JsonNode, fieldName: String, required: Boolean): StructField = {
+        val datatype = buildDataType(jsonSchema, fieldName)
+        val field = StructField(fieldName, datatype, nullable = !required)
+
+        val description = jsonSchema.get(descriptionField)
+        // Return field with comment if defined, without otherwise
+        if (description != null && description.isTextual) field.withComment(description.asText)
+        else field
+    }
+
+    /*
+     * Build a StructType from a JsonNode and a field-name for better logging (defaulting to `root`).
+     * The JsonNode is an object (checked in toSparkSchema and buildDataType) and is expected
+     * to contain a valid JSONSchema object-definition as subfields.
+     *
+     * This function recursively calls the buildStructField function to build every
+     * property of the defined object associated field.
+     */
+    private def buildStructType(jsonSchema: JsonNode, fieldName: String = "root"): StructType = {
+        val properties = jsonSchema.get(propertiesField)
+        if (properties == null || !properties.isObject)
+            throw new IllegalArgumentException(fieldName + " struct schema's \"properties\" is not an object.")
+        val requiredFieldList = jsonSchema.get(requiredField)
+        if (requiredFieldList != null && !requiredFieldList.isArray)
+            throw new IllegalArgumentException(fieldName + " struct schema's \"required\" is not an array.")
+        val requiredFieldSet = Try(requiredFieldList.elements().asScala.map(_.asText()).toSet).getOrElse(Set.empty)
+        val fields = properties.fields.asScala.toSeq
+        StructType(fields.map(mapEntry => {
+            val subfieldName = mapEntry.getKey
+            val subfieldNode = mapEntry.getValue
+            buildStructField(subfieldNode, subfieldName, requiredFieldSet.contains(subfieldName))
+        }))
+    }
+
+    /**
+     * Converts a JSON schema to a Spark StructType.
+     *
+     * Notes:
+     * - Only a subset of JSONSchema is supported.
+     * - object with additionalProperties only are converted to a Map.
+     *
+     * @param jsonSchema the JSONSchema root node
+     * @return the spark schema StructType
+     */
+    def toSparkSchema(jsonSchema: JsonNode): StructType = {
+        if (jsonSchema == null || jsonSchema.isNull)
+            throw new IllegalArgumentException("Schema root node shouldn't be null when building Spark Schema")
+        if (!jsonSchema.isObject || !jsonSchema.has(propertiesField))
+            throw new IllegalArgumentException("Schema root node should be an object with properties when building Spark Schema")
+        buildStructType(jsonSchema)
+    }
+}
---
+++ refinery-spark/src/test/scala/org/wikimedia/analytics/refinery/spark/sql/TestJsonSchemaConverter.scala
@@ -0,0 +1,273 @@
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.spark.sql.types._
+import org.scalatest.{Matchers, FlatSpec}
+import org.wikimedia.analytics.refinery.spark.sql.JsonSchemaConverter
+
+class TestJsonSchemaConverter extends FlatSpec with Matchers {
+
+  /**********************************************************
+   * Valid cases
+   */
+
+  val mapper = new ObjectMapper()
+
+  it should "convert a properly formatted json schema (boolean)" in {
+    val testSchema = """{"properties":{"test_boolean":{"type":"boolean"}}}"""
+    val expectedFields = Array(StructField("test_boolean", BooleanType, nullable = true))
+
+    val node = mapper.readTree(testSchema)
+    val sparkSchema = JsonSchemaConverter.toSparkSchema(node)
+
+    sparkSchema should not be null
+    sparkSchema.fields should equal(expectedFields)
+  }
+
+  it should "convert a properly formatted json schema (integer)" in {
+    val testSchema = """{"properties":{"test_integer":{"type":"integer"}}}"""
+    val expectedFields = Array(StructField("test_integer", IntegerType, nullable = true))
+
+    val node = mapper.readTree(testSchema)
+    val sparkSchema = JsonSchemaConverter.toSparkSchema(node)
+
+    sparkSchema should not be null
+    sparkSchema.fields should equal(expectedFields)
+  }
+
+  it should "convert a properly formatted json schema (number)" in {
+    val testSchema = """{"properties":{"test_number":{"type":"number"}}}"""
+    val expectedFields = Array(StructField("test_number", DoubleType, nullable = true))
+
+    val node = mapper.readTree(testSchema)
+    val sparkSchema = JsonSchemaConverter.toSparkSchema(node)
+
+    sparkSchema should not be null
+    sparkSchema.fields should equal(expectedFields)
+  }
+
+  it should "convert a properly formatted json schema (string)" in {
+    val testSchema = """{"properties":{"test_string":{"type":"string"}}}"""
+    val expectedFields = Array(StructField("test_string", StringType, nullable = true))
+
+    val node = mapper.readTree(testSchema)
+    val sparkSchema = JsonSchemaConverter.toSparkSchema(node)
+
+    sparkSchema should not be null
+    sparkSchema.fields should equal(expectedFields)
+  }
+
+  it should "convert a properly formatted json schema (array)" in {
+    val testSchema = """{"properties":{"test_array":{"type":"array","items":{"type":"string"}}}}"""
+    val expectedFields = Array(
+      StructField("test_array", ArrayType(StringType, containsNull = false), nullable = true))
+
+    val node = mapper.readTree(testSchema)
+    val sparkSchema = JsonSchemaConverter.toSparkSchema(node)
+
+    sparkSchema should not be null
+    sparkSchema.fields should equal(expectedFields)
+  }
+
+  it should "convert a properly formatted json schema (object-struct)" in {
+    val testSchema =
+      """{"properties":{"test_object":{"type":"object","properties":{
+        |"subfield1":{"type":"string"},"subfield2":{"type":"integer"}}}}}""".stripMargin
+    val subObjectSchema = StructType(Seq(
+      StructField("subfield1", StringType, nullable = true),
+      StructField("subfield2", IntegerType, nullable = true)))
+    val expectedFields = Array(
+      StructField("test_object", subObjectSchema, nullable = true))
+
+    val node = mapper.readTree(testSchema)
+    val sparkSchema = JsonSchemaConverter.toSparkSchema(node)
+
+    sparkSchema should not be null
+    sparkSchema.fields should equal(expectedFields)
+  }
+
+  it should "convert a properly formatted json schema (object-map)" in {
+    val testSchema =
+      """{"properties":{"test_map":{"type":"object",
+        |"additionalProperties":{"type":"number"}}}}""".stripMargin
+    val expectedFields = Array(
+      StructField("test_map", MapType(StringType, DoubleType, valueContainsNull = false), nullable = true))
+
+    val node = mapper.readTree(testSchema)
+    val sparkSchema = JsonSchemaConverter.toSparkSchema(node)
+
+    sparkSchema should not be null
+    sparkSchema.fields should equal(expectedFields)
+  }
+
+  it should "convert a properly formatted json schema with comment" in {
+    val testSchema = """{"properties":{"test_boolean":{"type":"boolean","description":"text comment"}}}"""
+    val expectedFields = Array(StructField("test_boolean", BooleanType, nullable = true).withComment("text comment"))
+
+    val node = mapper.readTree(testSchema)
+    val sparkSchema = JsonSchemaConverter.toSparkSchema(node)
+
+    sparkSchema should not be null
+    sparkSchema.fields should equal(expectedFields)
+  }
+
+  it should "convert a properly formatted json schema with required property" in {
+    val testSchema =
+      """{"properties":{"test_boolean":{"type":"boolean"}},
+        |"required":["test_boolean"]}""".stripMargin
+    val expectedFields = Array(StructField("test_boolean", BooleanType, nullable = false))
+
+    val node = mapper.readTree(testSchema)
+    val sparkSchema = JsonSchemaConverter.toSparkSchema(node)
+
+    sparkSchema should not be null
+    sparkSchema.fields should equal(expectedFields)
+  }
+
+  it should "convert a properly formatted complex json schema" in {
+    val testSchema =
+      """
+        |{
+        |  "properties": {
+        |    "test_boolean": {
+        |      "description": "test boolean comment",
+        |      "type": "boolean"
+        |    },
+        |    "test_object": {
+        |      "type": "object",
+        |      "properties": {
+        |        "subfield1": {
+        |          "description": "test test_object-subobject string comment",
+        |          "type": "string"
+        |        },
+        |        "subfield2": {
+        |          "description": "test test_object-subobject integer comment",
+        |          "type":"integer"
+        |        }
+        |      },
+        |      "required": [ "subfield2" ]
+        |    },
+        |    "test_array": {
+        |      "description": "test array comment",
+        |      "type": "array",
+        |      "items": {
+        |        "type": "object",
+        |        "properties": {
+        |          "array_subfield1": {
+        |            "description": "test test_array-subobject boolean comment",
+        |            "type": "boolean"
+        |          },
+        |          "array_subfield2": {
+        |            "description": "test test_array-subobject number comment",
+        |            "type":"number"
+        |          }
+        |        },
+        |        "required": [ "array_subfield1" ]
+        |      }
+        |    },
+        |    "test_array_map": {
+        |      "type": "array",
+        |      "items": {
+        |        "type": "object",
+        |        "additionalProperties": {
+        |            "type": "string"
+        |        }
+        |      }
+        |    }
+        |  },
+        |  "required": [ "test_boolean", "test_array" ]
+        |}""".stripMargin
+
+    val subObjectType = StructType(Seq(
+      StructField("subfield1", StringType, nullable = true)
+        .withComment("test test_object-subobject string comment"),
+      StructField("subfield2", IntegerType, nullable = false)
+        .withComment("test test_object-subobject integer comment")
+    ))
+    val subArrayType = ArrayType(StructType(Seq(
+      StructField("array_subfield1", BooleanType, nullable = false)
+        .withComment("test test_array-subobject boolean comment"),
+      StructField("array_subfield2", DoubleType, nullable = true)
+        .withComment("test test_array-subobject number comment")
+    )), containsNull = false)
+    val subArrayMapType = ArrayType(
+      MapType(StringType, StringType, valueContainsNull = false),
+      containsNull = false)
+
+    val expectedFields = Array(
+      StructField("test_boolean", BooleanType, nullable = false).withComment("test boolean comment"),
+      StructField("test_object", subObjectType, nullable = true),
+      StructField("test_array", subArrayType, nullable = false).withComment("test array comment"),
+      StructField("test_array_map", subArrayMapType, nullable = true)
+    )
+
+    val node = mapper.readTree(testSchema)
+    val sparkSchema = JsonSchemaConverter.toSparkSchema(node)
+
+    sparkSchema should not be null
+    sparkSchema.fields should equal(expectedFields)
+  }
+
+  /**********************************************************
+   * Error cases
+   */
+
+  it should "throw an IllegalArgumentException if properties is not defined" in {
+    val testSchema = """"required":["test_boolean"]}"""
+    val node = mapper.readTree(testSchema)
+    an[IllegalArgumentException] should be thrownBy JsonSchemaConverter.toSparkSchema(node)
+  }
+
+  it should "throw an IllegalArgumentException if type is not defined" in {
+    val testSchema = """{"properties":{"test_boolean":{"description":"empty"}}}"""
+    val node = mapper.readTree(testSchema)
+    an[IllegalArgumentException] should be thrownBy JsonSchemaConverter.toSparkSchema(node)
+  }
+
+  it should "throw an IllegalArgumentException if type is not valid" in {
+    val testSchema = """{"properties":{"test_boolean":{"type":"wrong"}}}"""
+    val node = mapper.readTree(testSchema)
+    an[IllegalArgumentException] should be thrownBy JsonSchemaConverter.toSparkSchema(node)
+  }
+
+  it should "throw an IllegalArgumentException if array doesn't defines item" in {
+    val testSchema = """{"properties":{"test_array":{"type":"array"}}}"""
+    val node = mapper.readTree(testSchema)
+    an[IllegalArgumentException] should be thrownBy JsonSchemaConverter.toSparkSchema(node)
+  }
+
+  it should "throw an IllegalArgumentException if array doesn't defines item type" in {
+    val testSchema = """{"properties":{"test_array":{"type":"array", "items":{"description":"empty"}}}}"""
+    val node = mapper.readTree(testSchema)
+    an[IllegalArgumentException] should be thrownBy JsonSchemaConverter.toSparkSchema(node)
+  }
+
+  it should "throw an IllegalArgumentException if object doesn't properties nor additionalProperties" in {
+    val testSchema = """{"properties":{"test_object":{"type":"object", "items":{"description":"empty"}}}}"""
+    val node = mapper.readTree(testSchema)
+    an[IllegalArgumentException] should be thrownBy JsonSchemaConverter.toSparkSchema(node)
+  }
+
+  it should "throw an IllegalArgumentException if object properties doesn't define type" in {
+    val testSchema = """{"properties":{"test_object":{"type":"object", "properties":{"p1":{"description":"empty"}}}}}"""
+    val node = mapper.readTree(testSchema)
+    an[IllegalArgumentException] should be thrownBy JsonSchemaConverter.toSparkSchema(node)
+  }
+
+  it should "throw an IllegalArgumentException if object additionalProperties doesn't define type" in {
+    val testSchema = """{"properties":{"test_object":{"type":"object", "additionalProperties":{"description":"empty"}}}}"""
+    val node = mapper.readTree(testSchema)
+    an[IllegalArgumentException] should be thrownBy JsonSchemaConverter.toSparkSchema(node)
+  }
+
+  it should "throw an IllegalArgumentException if root node is not an object" in {
+    val testSchema = """1"""
+    val node = mapper.readTree(testSchema)
+    an[IllegalArgumentException] should be thrownBy JsonSchemaConverter.toSparkSchema(node)
+  }
+
+  it should "throw an IllegalArgumentException if a type node is not an object" in {
+    val testSchema = """{"properties":{"test_boolean": false}}"""
+    val node = mapper.readTree(testSchema)
+    an[IllegalArgumentException] should be thrownBy JsonSchemaConverter.toSparkSchema(node)
+  }
+
+}
