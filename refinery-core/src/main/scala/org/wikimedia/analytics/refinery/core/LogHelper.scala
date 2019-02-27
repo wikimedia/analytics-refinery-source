1a03e5cd0da2c64fff75548185b3002346efcca4
Joseph Allemandou <joal@wikimedia.org>
2019-02-27 15:51:06 +0100

    Update LogHelper for parameterization
    This patch makes the LogHelper configuration property name overwritable
    in the trait so that it can be changed at trait-extension by overriding it.
    
    Change-Id: I7ca2fe124c68baa25974aac414d128a8774ccb32

--- refinery-core/src/main/scala/org/wikimedia/analytics/refinery/core/LogHelper.scala
+++ refinery-core/src/main/scala/org/wikimedia/analytics/refinery/core/LogHelper.scala
@@ -6,14 +6,25 @@ import org.apache.log4j.{Level, LogManager, Logger}
 /**
   * Helper class to get a logger with a property that can be set on the CLI,
   * named after the extending class.  When used with Refine and spark-submit, you can set
-  * --driver-java-options='-Djrefine.log.level=DEBUG' in local client mode,
-  * --conf 'spark.executor.extraJavaOptions=-Drefine.log.level=DEBUG' if using remote executors,
-  * --conf 'spark.driver.extraJavaOptions=-Drefine.log.level=DEBUG' if in remote drive mode (e.g. --master yarn).
+  * --driver-java-options='-Drefinery.log.level=DEBUG' in local client mode,
+  * --conf 'spark.executor.extraJavaOptions=-Drefinery.log.level=DEBUG' if using remote executors,
+  * --conf 'spark.driver.extraJavaOptions=-Drefinery.log.level=DEBUG' if in remote drive mode (e.g. --master yarn).
+  *
+  * You can overwrite logLevelConfigName to use a different configuration property
+  * to setup logging:
+  *
+  * class MyExampleClass extends LogHelper {
+  *   override val logLevelConfigName = "my.example.config.name"
+  * }
+  *
   */
 trait LogHelper {
+
+    val logLevelConfigName = "refinery.log.level"
+
     lazy val log: Logger = {
         val l = LogManager.getLogger(this.getClass.getName.split('.').last.split('$').last)
-        l.setLevel(Level.toLevel(System.getProperty("refine.log.level", "INFO"), Level.INFO))
+        l.setLevel(Level.toLevel(System.getProperty(logLevelConfigName, "INFO"), Level.INFO))
         l
     }
 }
