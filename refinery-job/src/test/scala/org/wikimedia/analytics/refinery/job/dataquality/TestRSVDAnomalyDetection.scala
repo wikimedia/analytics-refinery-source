d8eb4ee9f9bb833f43daa5fe501db29998e5b385
Marcel Ruiz Forns <mforns@wikimedia.org>
2021-07-23 20:56:15 +0200

    Simplify RSVD anomaly detection job for Airflow POC
    Bug: T285692
    Change-Id: Id65c031e65b452dd8178c17468bf5656e06715d0

--- refinery-job/src/test/scala/org/wikimedia/analytics/refinery/job/dataquality/TestRSVDAnomalyDetection.scala
+++ refinery-job/src/test/scala/org/wikimedia/analytics/refinery/job/dataquality/TestRSVDAnomalyDetection.scala
@@ -45,13 +45,12 @@ class TestRSVDAnomalyDetection extends FlatSpec with Matchers with DataFrameSuit
 
     it should "fill in gaps in incomplete time series" in {
         val gapsTimeSeries = Array(
-            ("2020-01-01T00:00:00Z", 1.0),
-            ("2020-01-02T00:00:00Z", 2.0),
-            ("2020-01-04T00:00:00Z", 3.0)
+            ("2020-01-01", 1.0),
+            ("2020-01-02", 2.0),
+            ("2020-01-04", 3.0)
         )
         val params = RSVDAnomalyDetection.Params(
-            granularity = Granularity.withName("daily"),
-            lastDataPointDt = "2020-01-05T00:00:00Z",
+            lastDataPointDt = "2020-01-05",
             defaultFiller = 0.0
         )
         val rsvd = new RSVDAnomalyDetection(spark, params)
@@ -59,17 +58,16 @@ class TestRSVDAnomalyDetection extends FlatSpec with Matchers with DataFrameSuit
         val filledTimeSeries = rsvd.fillInGaps(gapsTimeSeries)
 
         assert(filledTimeSeries.length == 5)
-        assert(filledTimeSeries(0) == ("2020-01-01T00:00:00Z", 1.0))
-        assert(filledTimeSeries(1) == ("2020-01-02T00:00:00Z", 2.0))
-        assert(filledTimeSeries(2) == ("2020-01-03T00:00:00Z", 0.0))
-        assert(filledTimeSeries(3) == ("2020-01-04T00:00:00Z", 3.0))
-        assert(filledTimeSeries(4) == ("2020-01-05T00:00:00Z", 0.0))
+        assert(filledTimeSeries(0) == ("2020-01-01", 1.0))
+        assert(filledTimeSeries(1) == ("2020-01-02", 2.0))
+        assert(filledTimeSeries(2) == ("2020-01-03", 0.0))
+        assert(filledTimeSeries(3) == ("2020-01-04", 3.0))
+        assert(filledTimeSeries(4) == ("2020-01-05", 0.0))
     }
 
     it should "not analyze too short timeseries" in {
         val shortTimeSeries = Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0)
         val params = RSVDAnomalyDetection.Params(
-            granularity = Granularity.withName("daily"),
             defaultFiller = 0.0,
             maxSparsity = 0.2,
             rsvdDimensions = 1,
@@ -84,7 +82,6 @@ class TestRSVDAnomalyDetection extends FlatSpec with Matchers with DataFrameSuit
     it should "not analyze too sparse timeseries" in {
         val shortTimeSeries = Array(1.0, 0.0, 3.0, 4.0, 5.0, 0.0, 7.0, 8.0, 9.0)
         val params = RSVDAnomalyDetection.Params(
-            granularity = Granularity.withName("hourly"),
             defaultFiller = 0.0,
             maxSparsity = 0.2,
             rsvdDimensions = 1,
