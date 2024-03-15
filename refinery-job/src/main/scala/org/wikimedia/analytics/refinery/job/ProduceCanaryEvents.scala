db12adf4ad067c6df7014335563e75eb1d3cf007
Joseph Allemandou <joal@wikimedia.org>
2024-03-15 22:56:06 +0100

    Update ProduceCanaryEvents job
    Update canary event producing job to work for single stream at once
    instead of looping over streams (airflow will do the looping), and
    make them time-idempotent by adding a timestamp parameter to fill in
    the meta.dt field on the generated canary-events.
    
    Bug: T341229
    Change-Id: I03e8218744ed2327a749a7c9f74099df80fcbfbf

--- refinery-job/src/main/scala/org/wikimedia/analytics/refinery/job/ProduceCanaryEvents.scala
+++ refinery-job/src/main/scala/org/wikimedia/analytics/refinery/job/ProduceCanaryEvents.scala
@@ -5,6 +5,7 @@ import scala.collection.JavaConverters._
 import scala.collection.immutable.ListMap
 import scala.collection.mutable
 import scala.util.Try
+import com.github.nscala_time.time.Imports._
 import org.apache.http.client.config.RequestConfig
 import org.wikimedia.analytics.refinery.tools.LogHelper
 import org.wikimedia.analytics.refinery.tools.config._
@@ -24,8 +25,8 @@ object ProduceCanaryEvents extends LogHelper with ConfigHelper {
       * Config class for use config files and args.
       */
     case class Config(
-        stream_names: Seq[String] = Seq(),
-        settings_filters: Map[String, String] = Map(),
+        stream_name: String,
+        timestamp: DateTime,
         schema_base_uris: Seq[String] = Seq(
             "https://schema.discovery.wmnet/repositories/primary/jsonschema",
             "https://schema.discovery.wmnet/repositories/secondary/jsonschema"
@@ -41,34 +42,24 @@ object ProduceCanaryEvents extends LogHelper with ConfigHelper {
         validate()
 
         /**
-          * Validates that configs as provided make sense.
-          * Throws IllegalArgumentException if not.
+          * Empty method to be reused if needed
           */
         private def validate(): Unit = {
-            if (stream_names.isEmpty && settings_filters.isEmpty) {
-                throw new IllegalArgumentException(
-                    "Must provide one of --stream_names or --settings_filters to target " +
-                    "streams for which to produce canary events."
-                )
-            }
         }
     }
 
     object Config {
         // This is just used to ease generating help message with default values.
         // Required configs are set to dummy values.
-        val default: Config = Config(Seq("_EXAMPLE_STREAM_"))
+        val default: Config = Config("_EXAMPLE_STREAM_", DateTime.now())
 
         val propertiesDoc: ListMap[String, String] = ListMap(
-            "stream_names" ->"""
-                |If provided, only these streams will have canary events produced.
-                |Must provide at least one of --stream_names or --settings_filters.
+            "stream_name" ->"""
+                |Only this stream will have canary events produced.
                 |""".stripMargin,
-            "settings_filters" -> """
-                |If provided, only streams that have these settings in Event Stream Config
-                |will have canary events produced.
-                |Must provide at least one of --stream_names or --settings_filters.
-                |Should be provided as settingA:valueA,settingB:valueB
+            "timestamp" ->"""
+                |Timestamp in ISO format for which the canary events will be produced.
+                |The meta.dt field will be populated with this value.
                 |""".stripMargin,
             "schema_base_uris" -> s"""
                 |Event schemas will be loaded from these URIs.
@@ -81,26 +72,26 @@ object ProduceCanaryEvents extends LogHelper with ConfigHelper {
                 |endpoint. Default: ${default.event_stream_config_uri}
                 |""".stripMargin,
             "event_service_config_uri" -> s"""
-               |URI or local path to a YAML or JSON config file that
-               |maps event intake service name to an HTTP URI.  This
-               |will be used to determine the event intake service URI
-               |to which a canary event should be produced.
-               |""".stripMargin,
-            "use_wikimedia_http_client" ->
-                s"""(deprecated) If true, wikimedia-event-utilities WikimediaDefaults.WIKIMEDIA_HTTP_CLIENT
-               |will be used when making http request to get event stream config and to post
-               |canary events.  This should always be used in production to properly route
-               |to internal production API endpoints.  Default: ${default.use_wikimedia_http_client}
-               |""".stripMargin,
-            "http_routes" ->
-                s"""Set the list of http routes to use when making http requests.
-                   |Form is: source1=target1,source2=target2.
-                   |E.g. meta.wikimedia.org=https://api-ro.discovery.wmnet
-                   |""".stripMargin,
-            "http_timeout" ->
-                s"""Set the timeout in seconds for http requests.
-                   |Default is 10. Ignored if use_wikimedia_http_client is true.
-                   |""".stripMargin,
+                |URI or local path to a YAML or JSON config file that
+                |maps event intake service name to an HTTP URI.  This
+                |will be used to determine the event intake service URI
+                |to which a canary event should be produced.
+                |""".stripMargin,
+            "use_wikimedia_http_client" ->s"""
+                |(deprecated) If true, wikimedia-event-utilities WikimediaDefaults.WIKIMEDIA_HTTP_CLIENT
+                |will be used when making http request to get event stream config and to post
+                |canary events.  This should always be used in production to properly route
+                |to internal production API endpoints.  Default: ${default.use_wikimedia_http_client}
+                |""".stripMargin,
+            "http_routes" ->s"""
+                |Set the list of http routes to use when making http requests.
+                |Form is: source1=target1,source2=target2.
+                |E.g. meta.wikimedia.org=https://api-ro.discovery.wmnet
+                |""".stripMargin,
+            "http_timeout" ->s"""
+                |Set the timeout in seconds for http requests.
+                |Default is 10. Ignored if use_wikimedia_http_client is true.
+                |""".stripMargin,
             "dry_run" -> s"""
                 |Don't actually produce any canary events, just
                 |output the events that would have been produced.
@@ -113,21 +104,18 @@ object ProduceCanaryEvents extends LogHelper with ConfigHelper {
         val usage: String =
             """
               |Produces canary events to event intake services.
-              |This job produces events for the selected streams and their
-              |corresponding event intake services, and then exits.
-              |Streams are discovered using the MediaWiki EventStreamConfig API,
-              |and canary events for those streams are created from
-              |event schema examples.
+              |This job produces events for the selected stream's event intake services,
+              |using the timestamp provided as meta.dt value, and then exits.
+              |Canary events for the stream are created from event schema examples.
               |Canary events will be POSTed to event service URIs determined
               |by mapping the destination_event_service stream config setting
               |to a set of datacenter specific URIs.
               |
               |Example:
-              |  # Produce canary events for all streams that have canary_events_enabled
-              |  # setting set to true in stream config.
+              |  # Produce canary events for NavigationTiming stream 2024-03-15T19:05:00
               |  java -cp refinery-job.jar \
               |     org.wikimedia.analytics.refinery.job.ProduceCanaryEvents \
-              |     --settings_filters=canary_events_enabled:true
+              |     --stream_name navigationtimining --timestamp 2024-03-15T19:05:00
               |"""
 
         /**
@@ -159,15 +147,18 @@ object ProduceCanaryEvents extends LogHelper with ConfigHelper {
         }
         val config = Config(args)
 
-        val success = apply(config)
-        sys.exit(if (success) 0 else 1)
+        val result = apply(config)
+
+        if (result.isSuccess) {
+            log.info(s"Succeeded producing canary events to ${config.stream_name}.")
+        } else {
+            log.error("Encountered exception in produceCanaryEvents.", result.failed.get)
+        }
+
+        sys.exit(if (result.isSuccess) 0 else 1)
     }
 
-    /**
-      * Produces canary events (or just logs them if config.dry_run).
-      * @return
-      */
-    def apply(config: Config): Boolean = {
+    def init(config: Config): (EventStreamFactory, BasicHttpClient) = {
         // Use the WIKIMEDIA_HTTP_CLIENT if in WMF production.
         // This routes e.g. meta.wikimedia.org -> api-ro.wikimedia.org.
         val httpClient = if (config.use_wikimedia_http_client) {
@@ -212,102 +203,33 @@ object ProduceCanaryEvents extends LogHelper with ConfigHelper {
 
         // EventStreamFactory using eventSchemaLoader and eventStreamConfig
         val eventStreamFactory = EventStreamFactory.builder()
-          .setEventSchemaLoader(eventSchemaLoader)
-          .setEventStreamConfig(eventStreamConfig)
-          .build()
+            .setEventSchemaLoader(eventSchemaLoader)
+            .setEventStreamConfig(eventStreamConfig)
+            .build()
 
-        val targetEventStreams = getTargetEventStreams(
-            eventStreamFactory,
-            config.stream_names,
-            config.settings_filters
-        )
-
-        if (targetEventStreams.isEmpty) {
-            log.warn(
-                "No event streams match the provided stream_names and settings_filters. " +
-                "No canary events will be produced."
-            )
-            false
-        } else {
-            val canaryEventProducer = new CanaryEventProducer(eventStreamFactory, httpClient)
-
-            // Instead of producing all streams at once, produce them one at a time.
-            // This is less efficient (results in an HTTP post for every stream (actually 2,
-            // one for each DC)), but allows us to handle errors for each stream separately.
-            // See: https://phabricator.wikimedia.org/T270138
-            val results: Seq[Try[Boolean]] = targetEventStreams.map { eventStream =>
-                Try(
-                    // Since we often receive 500 from eventgates, let's mitigate the false alerts with some retries.
-                    retry(3, "Retrying produceCanaryEvents"){
-                        produceCanaryEvents(canaryEventProducer, Seq(eventStream), config.dry_run)
-                    }
-                )
-            }
-
-            // Log any unexpected exceptions after we are done producing events.
-            val exceptions = results.filter(_.isFailure).map(_.failed.get)
-            exceptions.foreach(log.error("Encountered exception in produceCanaryEvents.", _))
-
-            // Failures have already been logged.
-            val failures = results.filter(r => r.isSuccess && !r.get)
-            val successes = results.filter(r => r.isSuccess && r.get)
-
-            if (successes.nonEmpty) {
-                log.info(
-                    "Succeeded producing canary events to " +
-                    s" ${successes.length} / ${targetEventStreams.length} streams."
-                )
-            }
-
-            if (exceptions.nonEmpty) {
-                log.error(
-                    "Encountered unexpected exceptions when producing canary events to " +
-                    s" ${exceptions.length} / ${targetEventStreams.length} streams."
-                )
-            }
-
-            if (failures.nonEmpty) {
-                log.error(
-                    "Encountered failures when producing canary events to " +
-                    s"${failures.length} / ${targetEventStreams.length} streams."
-                )
-            }
-
-            // Return true if there were no unexpected exceptions or failures
-            exceptions.isEmpty && failures.isEmpty
-        }
-
+        (eventStreamFactory, httpClient)
     }
 
     /**
-      * Uses eventStreamFactory to get EventStreams for streamNames that have stream configs
-      * with settings that match what is provided in settingsFilters.
-      * If neither streamNames or settingsFilters are provided, all known EventStreams
-      * will be returned.
-      *
-      * Since settingsFilters must all be strings, this only allows filtering
-      * on string stream config settings, or at least ones for which JsonNode.asText()
-      * returns something sane (which is true for most primitive types).
+      * Produces canary events (or just logs them if config.dry_run).
+      * @return
       */
-    def getTargetEventStreams(
-        eventStreamFactory: EventStreamFactory,
-        streamNames: Seq[String] = Seq(),
-        settingsFilters: Map[String, String] = Map()
-    ): Seq[EventStream] = {
-        // Instantiate event streams either by stream names, or get them all.
-        val eventStreams: Seq[EventStream] = { streamNames match {
-            case Seq() => eventStreamFactory.createAllCachedEventStreams
-            case _ => eventStreamFactory.createEventStreams(streamNames.asJava)
-        }}.asScala.toSeq
+    def apply(config: Config): Try[Unit] = {
+        val (eventStreamFactory, httpClient) = init(config)
+        val targetEventStream: EventStream = eventStreamFactory.createEventStream(config.stream_name)
 
-        // If settingsFilters are provided, filter for eventStreams that have those settings.
-        // This will just return eventStreams as is if settingsFilters is an empty Map.
-        eventStreams.filter(eventStream => {
-            settingsFilters.forall({ case (settingKey, filterSetting) =>
-                val streamSetting = eventStream.getSetting(settingKey)
-                streamSetting != null && filterSetting == streamSetting.asText()
-            })
-        })
+        Try(
+            if (targetEventStream == null) {
+                throw new IllegalArgumentException(s"No event stream match the provided stream_name ${config.stream_name}.")
+            } else {
+                val canaryEventProducer = new CanaryEventProducer(eventStreamFactory, httpClient)
+
+                // Since we often receive 500 from eventgates, let's mitigate the false alerts with some retries.
+                retry(3, "Retrying produceCanaryEvents"){
+                    produceCanaryEvents(canaryEventProducer, targetEventStream, config.timestamp, config.dry_run)
+                }
+            }
+        )
     }
 
     /**
@@ -315,11 +237,12 @@ object ProduceCanaryEvents extends LogHelper with ConfigHelper {
       */
     def produceCanaryEvents(
         canaryEventProducer: CanaryEventProducer,
-        eventStreams: Seq[EventStream],
+        eventStream: EventStream,
+        timestamp: DateTime,
         dryRun: Boolean = false
     ): Boolean = {
         // Map of event service URI -> List of canary events to produce to that event service.
-        val uriToCanaryEvents = canaryEventProducer.getCanaryEventsToPostForStreams(eventStreams.asJava)
+        val uriToCanaryEvents = canaryEventProducer.getCanaryEventsToPostForStreams(Seq(eventStream).asJava, timestamp)
 
         // Build a description string of the POST requests and events for logging.
         val canaryEventsPostDescription = uriToCanaryEvents.asScala.toList.map({
@@ -329,8 +252,7 @@ object ProduceCanaryEvents extends LogHelper with ConfigHelper {
 
         log.info(
             {if (dryRun) "DRY-RUN, would have produced " else "Producing "} +
-            "canary events for streams:\n  " +
-            eventStreams.map(_.streamName).mkString("\n  ") + "\n" +
+            s"canary events for stream ${eventStream}:\n" +
             canaryEventsPostDescription + "\n"
         )
 
